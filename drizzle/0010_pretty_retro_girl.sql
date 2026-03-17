63658f15c3098d76622e8782bdb218ccd02af801
aarmicsta <156771144+aarmicsta@users.noreply.github.com>
2026-03-17 14:02:53 +0000

    drizzle generate

---
+++ drizzle/0010_pretty_retro_girl.sql
@@ -0,0 +1,12 @@
+ALTER TABLE `playable_classes` DROP COLUMN `base_health`;--> statement-breakpoint
+ALTER TABLE `playable_classes` DROP COLUMN `base_attack`;--> statement-breakpoint
+ALTER TABLE `playable_classes` DROP COLUMN `base_defense`;--> statement-breakpoint
+ALTER TABLE `playable_classes` DROP COLUMN `base_speed`;--> statement-breakpoint
+ALTER TABLE `playable_classes` DROP COLUMN `base_intelligence`;--> statement-breakpoint
+ALTER TABLE `playable_classes` DROP COLUMN `base_charisma`;--> statement-breakpoint
+ALTER TABLE `playable_species` DROP COLUMN `base_health`;--> statement-breakpoint
+ALTER TABLE `playable_species` DROP COLUMN `base_attack`;--> statement-breakpoint
+ALTER TABLE `playable_species` DROP COLUMN `base_defense`;--> statement-breakpoint
+ALTER TABLE `playable_species` DROP COLUMN `base_speed`;--> statement-breakpoint
+ALTER TABLE `playable_species` DROP COLUMN `base_intelligence`;--> statement-breakpoint
+ALTER TABLE `playable_species` DROP COLUMN `base_charisma`;
